97115214d27e86fa1c94816e5c55d4dd4569c42e
Евгения Костарева <eugenia@mac-book-air-2020.local>
2022-09-27 22:48:10 +0300

    add prev version

---
+++ get_next_line_bonus.c
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlarra <mlarra@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/17 15:41:29 by mlarra            #+#    #+#             */
+/*   Updated: 2022/09/27 22:43:39 by mlarra           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+char	*ft_reading_buf(int fd, char *buf, char *tail)
+{
+	int		nbytes;
+
+	nbytes = 1;
+	while (nbytes != 0 && ft_strchr(tail, '\n') == NULL)
+	{
+		nbytes = read(fd, buf, BUFFER_SIZE);
+		if (nbytes < 0)
+		{
+			free ((void *)buf);
+			return ((void *)0);
+		}
+		buf[nbytes] = '\0';
+		if (tail)
+			tail = ft_strjoin(tail, buf);
+		else
+			tail = ft_substr(buf, 0, nbytes);
+	}
+	free((void *)buf);
+	return (tail);
+}
+
+char	*ft_get_line(char *tail)
+{
+	int		i;
+	char	*dest;
+
+	i = 0;
+	while (tail[i] && tail[i] != '\n')
+		i++;
+	if (tail[i] == '\n')
+		i++;
+	dest = ft_substr(tail, 0, i);
+	return (dest);
+}
+
+char	*ft_get_tail(char *tail)
+{
+	int		i;
+	char	*ret_tail;
+
+	i = 0;
+	while (tail[i] && tail[i] != '\n')
+		i++;
+	i++;
+	ret_tail = ft_substr(tail, i, ft_strlen(tail));
+	free ((void *)tail);
+	return (ret_tail);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*buf;
+	char		*dest;
+	static char	*tail[1025];
+
+	if (fd < 0 || BUFFER_SIZE < 1)
+		return ((void *)0);
+	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buf)
+		return ((void *)0);
+	tail[fd] = ft_reading_buf(fd, buf, tail[fd]);
+	if (!tail[fd] || tail[fd][0] == '\0')
+	{
+		free((void *)tail[fd]);
+		tail[fd] = 0;
+		return ((void *)0);
+	}
+	dest = ft_get_line(tail[fd]);
+	if (!dest || dest[0] == '\0')
+	{
+		free ((void *)dest);
+		return ((void *)0);
+	}
+	tail[fd] = ft_get_tail(tail[fd]);
+	return (dest);
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlarra <mlarra@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/17 15:41:56 by mlarra            #+#    #+#             */
+/*   Updated: 2022/09/27 22:43:54 by mlarra           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 20
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+size_t	ft_strlen(const char *s);
+#endif
